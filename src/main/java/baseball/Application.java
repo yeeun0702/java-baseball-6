a21aabad8251efe326c0a8efe630e5ca753381ed
yeeun0702 <kimyeeun0702@naver.com>
2024-03-31 14:22:23 +0900

    1차

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,38 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringBuilder sb = new StringBuilder(); // 문자열을 합쳐줌
+
+
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        for (Integer i : computer) {
+            sb.append(i);
+        }
+
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+
+        String userNum = Console.readLine();
+        String computerNum = sb.toString();
+
+
+
+
+
     }
 }
